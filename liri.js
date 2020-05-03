224bbd97600aa14b91ed2165702f6c98ba76652b
jonmeidell <jonmeidell@gmail.com>
2020-05-02 21:12:11 -0600

    initial file creation

---
+++ liri.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+// You should then be able to access your keys information like so
+// var spotify = new Spotify(keys.spotify);
+
+
+
+
+// Make it so liri.js can take in one of the following commands:
+
+
+// concert-this
+
+
+// spotify-this-song
+
+
+// movie-this
+
+
+// do-what-it-says
