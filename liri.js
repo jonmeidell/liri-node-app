0603f2681d1c9026a31995f8a65ca4b4d641a0d3
jonmeidell <jonmeidell@gmail.com>
2020-05-09 21:43:27 -0600

    added bandsintown funtion

--- liri.js
+++ liri.js
@@ -1,19 +1,33 @@
 require("dotenv").config();
+const fs = require("fs");
+const moment = require("moment");
+const Spotify = new Spotify(keys.spotify);
+const keys = require("./keys.js");
 
-// You should then be able to access your keys information like so
-// var spotify = new Spotify(keys.spotify);
+let artistName = function(artist) {
+    return artist.name;
+};
+// bandsintown
+let concertThis = function (artist) {
+    let query = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
-
-
-
-// Make it so liri.js can take in one of the following commands:
-
-
-// concert-this
-
-
+    axios.get(query).then(
+        function (response) {
+            const json = response.data;
+            if (!json.length) {
+                console.log("Could not find any upcoming shows for " + artist);
+                return;
+            }
+            console.log("Shows coming soon for " + artist + " are: ");
+            for (var i = 0; i < json.length; i++) {
+                let show = json[i];
+                console.log("Show in: " show.venue.city + ", "(show.venue.region || show.venue.country) + "at " + show.venue.name + " starting at " + moment(show.datetime).format("MM/DD/YYYY")
+                );
+            }
+        }
+    );
+};
 // spotify-this-song
-
 
 // movie-this
 
