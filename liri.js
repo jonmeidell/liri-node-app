e3d0ea811239010002a42ab1c86a4379071329d3
jonmeidell <jonmeidell@gmail.com>
2020-05-10 21:30:55 -0600

    added Spotify and movie search functions

--- liri.js
+++ liri.js
@@ -4,7 +4,7 @@ const moment = require("moment");
 const Spotify = new Spotify(keys.spotify);
 const keys = require("./keys.js");
 
-let artistName = function(artist) {
+let artistName = function (artist) {
     return artist.name;
 };
 // bandsintown
@@ -21,15 +21,55 @@ let concertThis = function (artist) {
             console.log("Shows coming soon for " + artist + " are: ");
             for (var i = 0; i < json.length; i++) {
                 let show = json[i];
-                console.log("Show in: " show.venue.city + ", "(show.venue.region || show.venue.country) + "at " + show.venue.name + " starting at " + moment(show.datetime).format("MM/DD/YYYY")
+                console.log("Show in: " + show.venue.city + ", "(show.venue.region || show.venue.country) + "at " + show.venue.name + " starting at " + moment(show.datetime).format("MM/DD/YYYY")
                 );
             }
         }
     );
 };
 // spotify-this-song
+let spotifyThis = function (songName) {
+    if (songName === undefined) {
+        songName = "Let it Be";
+    };
 
+    spotify.search(
+        {
+            type: 'track',
+            query: songName,
+        },
+        function (err, data) {
+            if (err) {
+                console.log("Error!");
+                return;
+            }
+
+            let songSelected = data.tracks.items;
+            for (var i = 0; i < songSelected.length; i++) {
+                console.log("Artist: " + songSelected[i].artist.map(artistName), "Song name: " + songSelected[i].name, "Album: " + songSelected[i].album.name);
+            };
+        }
+    );
+};
 // movie-this
+let movieThis = function (movieName) {
+    if (movieName === undefined) {
+        movieName = "Dark City";
+    };
 
+    let urlMovie = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
 
+    axios.get(urlMovie).then(
+        function (response) {
+            let jsonData = response.data;
+            console.log("Title: " + jsonData.Title);
+            console.log("Year: " + jsonData.Year);
+            console.log("Rated: " + jsonData.Rated);
+            console.log("IMDB Rating: " + jsonData.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value);
+            console.log("Plot: " + jsonData.Plot);
+            console.log("Actors: " + jsonData.Actors);
+        }
+    );
+};
 // do-what-it-says
